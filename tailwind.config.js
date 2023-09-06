aaff332996449587157b51db74bbdacc159390d9
Lucas Rocha <lucas_rocha.14@outlook.com>
2023-09-05 21:51:26 -0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,86 @@
-import plugin from 'tailwindcss/plugin';
+import plugin from 'tailwindcss/plugin'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-100': '#f3f3f3',
+
+        'blue-100': '#4D76D6',
+        'blue-200': '#283E70',
+        'blue-300': '#4D5975',
+        'blue-400': '#2C3342',
+        'blue-500': '#0F172A',
+
+        primary: '#050816',
+        secondary: '#aaa6c3',
+        tertiary: '#151030',
+        'black-100': '#100d25',
+        'black-200': '#090325',
+        flashWhite: '#f4f4f6',
+        platinum: '#e6e6e9',
+        platinumLight: '#f4f4f5',
+        timberWolf: '#d4d4d8',
+        taupe: '#9999a1',
+        silver: '#d6d6d6',
+        dim: '#66666e',
+        battleGray: '#858585',
+        french: '#b5b5ba',
+        night: '#141414',
+        jet: '#292929',
+        jetLight: '#333333',
+        jetGray: '#6d6d74',
+        richBlack: '#2e2e2e',
+        onyx: '#5b5b5b',
+
+        eerieBlack: '#1f1f1f',
+      },
+      boxShadow: {
+        card: '0px 35px 120px -15px #1f1f1f',
+        cardLight: '0px 19px 38px #eaeaec, 0px 15px 12px #eaeaec',
+      },
+      screens: {
+        xs: '450px',
+        sm: '640px',
+        md: '768px',
+        xmd: '900px',
+        lg: '1025px',
+        xl: '1280px',
+        '2xl': '1536px',
+        '3xl': '1800px',
+      },
+      backgroundImage: {
+        hero: 'linear-gradient(315deg, #0F172A 10%, #283E70 100%)',
+        type01:
+          'linear-gradient(180deg, #0F172A 0%, #0F172A 0.01%, #121C33 100%);',
+        type02: 'linear-gradient(0deg, #0F172A 0%, #121C33 100%)',
+        experience:
+          "linear-gradient(135deg, rgba(244,244,246,0.5) 60%, rgba(10,10,10,0.2) 100%),url('/src/assets/backgrounds/white-abstract.png')",
+        experienceLight:
+          'linear-gradient(137deg, rgba(244,244,246,0.5) 60%, rgba(10,10,10,0.9) 60%)',
+        // hero: 'linear-gradient(135deg, rgba(244,244,246,0.8) 60%, rgba(10,10,10,0.95) 60%)',
+        // 'hero-mobile':
+        //   'linear-gradient(137deg, rgba(244,244,246,0.8) 60%, rgba(10,10,10,1) 60%)',
+        tech: "linear-gradient(165deg, rgba(20,20,20,0.8) 100%, rgba(109,109,116,0.8) 100%), url('/src/assets/backgrounds/nairobi.png')",
+      },
+      fontFamily: {
+        arenq: ['Arenq'],
+        beckman: ['Beckman'],
+        mova: ['Mova'],
+        overcameBold: ['Overcame Bold'],
+        overcameOutline: ['Overcame Outline'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     // plugin(function({ addBase, theme }) {
